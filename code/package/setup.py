147f7bbff25798858e746b3c18e7e9d1b9825217
Lennart Wittkuhn <lennart.wittkuhn@tutanota.com>
2024-11-26 13:37:45 +0100

    Add package infrastructure

---
+++ code/package/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='preproc',
+      version='latest',
+      description='Highspeed Fast Preprocessing Package',
+      url='https://github.com/lnnrtwttkhn/highspeed-fast-preproc/',
+      author='Lennart Wittkuhn',
+      author_email='lennart.wittkuhn@uni-hamburg.de',
+      license='MIT',
+      packages=['preproc'],
+      zip_safe=False)
